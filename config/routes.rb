2a198fe8f840444356173c9f0ff8352c5083ef61
defektive <defektive@users.noreply.github.com>
2014-11-27 21:17:50 -0700

    api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'channel/' => 'channel#index'
-  get 'channel/:name' => 'channel#view'
-  patch 'channel/:name' => 'channel#update'
-  put 'channel/:name' => 'channel#update'
-
+  namespace :api do
+    get 'channel/' => 'channel#index'
+    get 'channel/:name' => 'channel#view'
+    patch 'channel/:name' => 'channel#update'
+    put 'channel/:name' => 'channel#update'
+  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
