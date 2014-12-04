89552bb4eeb17ef9764f75210027c419015e78a3
defektive <defektive@users.noreply.github.com>
2014-12-04 02:48:13 +0000

    Change port

--- app/controllers/channel_controller.rb
+++ app/controllers/channel_controller.rb
@@ -21,7 +21,7 @@ class ChannelController < ApplicationController
   # PATCH/PUT /outputs/1
   # PATCH/PUT /outputs/1.json
   def update
-    m = MonoMatrix.new "/dev/ttyUSB0"
+    m = MonoMatrix.new "/dev/ttyAMA0"
     m.switch params[:name], channel_params[:input].to_i
     head :no_content
   end
